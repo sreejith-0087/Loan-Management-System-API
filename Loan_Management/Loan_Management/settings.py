e6f87b11bb72a34bcdedba0c39ffcc99c0b6d0c9
sreejith-0087 <sreejith.s.8700@gmail.com>
2025-03-02 17:33:03 +0530

    Configured PostgreSQL for Render

--- Loan_Management/Loan_Management/settings.py
+++ Loan_Management/Loan_Management/settings.py
@@ -12,12 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from decouple import config
 from pathlib import Path
 from datetime import timedelta
-import os
-import dj_database_url
-from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
